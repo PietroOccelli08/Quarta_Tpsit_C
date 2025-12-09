82bdddcc30c7bb1c225af3f15b472aa67191f16a
PietroOccelli08 <pietro.occelli@itiscuneo.eu>
2025-12-09 17:45:10 +0100

    esercizi sulle liste in C

---
+++ liste_lab.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Nodo{
+    int key;
+    struct Nodo* next;
+}Node;
+
+int main() {
+    
+
+}
--- liste_rubrica.c
+++ liste_rubrica.c
@@ -7,25 +7,40 @@ fatta con una lista, con nome e numero
 #include <string.h>
 
 typedef struct {
-    char* nome;
-    char* numero;
+    char* name;
+    char* number;
     struct Node* next;
 } Node;
 
+Node* createNode(char* name, char* number, Node* prec){
+    Node* new = (Node*)malloc(sizeof(Node));
+    prec->next = new;
+
+    new->name = strdup(name);
+    new->number = strdup(number);
+    new->next = NULL;
+
+    return new;
+}
+
+void freeNode(Node* node, Node* prec){
+    prec->next = node->next;
+    free(node->name);
+    free(node->number);
+    free(node);
+}
+
 int main() {
     Node* head = (Node*)malloc(sizeof(Node));
-    char* nome = "Pietro";
-    char* numero = "3393222064";
+    char* name = "Pietro";
+    char* number = "3393222064";
 
-    head->nome = strdup(nome);
-    head->numero = strdup(numero);
+    head->name = strdup(name);
+    head->number = strdup(number);
     head->next = NULL;
 
-    printf("%s %s", head->nome, head->numero);
-
-    free(head->nome);
-    free(head->next);
-    free(head);
+    Node* person1 = createNode("Frank", "3712809694", head);
+    printf("%s %s %p", head->name, head->number, head->next);
 
     return 0;
 
